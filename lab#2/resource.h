5387dfe6fd4448d343c9baffa9318a8dfec85401
oxanadunav <oxana.dunav@gmail.com>
2016-03-02 20:57:26 +0200

    add lab#2

---
+++ lab#2/resource.h
@@ -0,0 +1,32 @@
+#define IDC_LIST_BOX            100
+#define IDC_NEW_ITEM            101
+
+#define IDC_ADD_BUTTON          103
+#define IDC_REMOVE_BUTTON       104
+#define IDC_CLEAR_BUTTON        105
+
+#define IDC_BACKGROUND_SCROLL   106
+#define IDC_HEIGHT_SCROLL       107
+#define IDC_WIDTH_SCROLL        108
+
+#define IDC_LABEL1              109
+#define IDC_LABEL2              110
+#define IDC_LABEL3              111
+
+
+#define IDM_MENU 200
+#define ID_EXIT 201
+#define ID_DATA 202
+#define ID_ABOUT 203
+
+#define IDI_ICON 300
+
+#define IDD_DIALOG 400
+#define IDC_STATIC 401
+
+#define HK_EXIT 500
+#define HK_ABOUT 501
+
+
+
+
